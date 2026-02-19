71ae052d41918f1894bd6af7d8977522fc3740cc
nguyenhaley <130112171+nguyenhaley@users.noreply.github.com>
2026-02-18 21:42:04 -0800

    more changes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // fullpage js logic
 $(document).ready(function () {
   $("#fullpage").fullpage({
-    anchors: ["welcome-page", "about", "contact"],
+    anchors: ["welcome-page", "about", "project", "contact"],
     navigation: true,
     navigationPosition: "right",
-    navigationToolTips: ["Welcome", "About Me", "Contact Me"],
+    navigationToolTips: ["Welcome", "About Me", "Projects", "Contact Me"],
     showActiveTooltip: true,
     scrollOverflow: true,
   });
